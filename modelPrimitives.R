f755795853b00e5efeb017033cc2230a4a0b20de
committed1960 <committed.committed@gmail.com>
2018-08-26 23:41:58 +0100

    Working code - 3 players, 3 states Different beliefs Mu and constant confidence alpha

--- modelPrimitives.R
+++ modelPrimitives.R
@@ -1,25 +1,53 @@
-###  Number of goods
-J = 5
+###  Number of states
+J <- 3
 
 ### Number of players
-I = 3
+I <- 3
 
 ###  Wealth
 W <- matrix(c(4,5,6))
 
 ### Beliefs
 Mu <- as.matrix(t(rbind(
-                  c(0.1,0.05,0.15), c(0.2, 0.2, 0.2), c(0.3, 0.2, 0.35), c(0.05, 0.1, 0.05), c(0.35, 0.45, 0.25)
+                  c(0.1,0.5,0.2), c(0.6, 0.3, 0.2), c(0.3, 0.2, 0.6)
                     )))
 
-###  Prices
-P <- matrix(1/J,J,1)
+#if apply(Mu, 1, sum)) <> c(1,1,1) then stop
+
+###  Certainty
+Alpha <- as.matrix(c(0.5,0.95,0.5))
+
+### Initial quantity
+Q <- Mu
 
 
+#### Find prices from quantities
 
-### Quantities
-Q <- matrix(1/I, I, J)
+for (t in 1:30)
+{
+xP <- solve(Q) %*% W
+P <- xP/sum(xP)
 
+## check
+#max((Q %*% P) / W) - min((Q %*% P) / W)
 
-### p^T Q^^ = 1
-### Q p = w
+### Find quantities from prices
+
+yP <- as.matrix(t(rbind(
+  c(1/P[1],0,0), c(0, 1/P[2], 0), c(0, 0, 1/P[3])
+)))
+
+## mu alpha q^{alpha -1} = 1 / p
+yQ <- 0.5*(Mu%*%yP)
+
+xQ <-(yQ)^{1/(1 - 0.5)}
+
+# check
+#  Mu[1,2]*0.5*xQ[1,2]^{0.5 - 1} - (P[2])
+
+normXQ <- colSums(xQ)
+
+Q <- xQ
+Q[,1] <- xQ[,1]/normXQ[1]
+Q[,2] <- xQ[,2]/normXQ[2]
+}
